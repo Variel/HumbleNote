89cf8b2c17ecae2f084ea9b35652ce8feae6dee0
이예찬 <blackmagic2@naver.com>
2022-11-26 23:20:10 +0900

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -27,8 +27,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-  app.UseSwagger();
-  app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
