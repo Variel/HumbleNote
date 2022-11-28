9356ac3fe9af17e5d864b6d20aa0e86f67136c2d
이예찬 <blackmagic2@naver.com>
2022-11-28 18:30:28 +0900

    list children 추가

--- Domain/Models/Results/CreateNoteResult.cs
+++ Domain/Models/Results/CreateNoteResult.cs
@@ -5,23 +5,23 @@ namespace HumbleNote.Domain.Models.Results;
 
 public class CreateNoteResult
 {
-    public CreateNoteStatus Status { get; set; }
-    public NoteDto? Note { get; set; }
+  public CreateNoteStatus Status { get; set; }
+  public NoteDto? Note { get; set; }
 
-    public CreateNoteResult(CreateNoteStatus status, Note? note = default)
+  public CreateNoteResult(CreateNoteStatus status, Note? note = default)
+  {
+    Status = status;
+
+    if (note != null)
     {
-        Status = status;
-
-        if (note != null)
-        {
-            Note = new NoteDto(note);
-        }
+      Note = new NoteDto(note);
     }
+  }
 }
 
 public enum CreateNoteStatus
 {
-    Succeeded,
-    ParentNotFound,
-    OldVersionNotFound
+  Succeeded,
+  ParentNotFound,
+  OldVersionNotFound
 }
